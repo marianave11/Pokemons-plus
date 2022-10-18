d5ab12453f3b21ef040b9e25b818e73ce4e237bf
marianave11 <mvalencia@lulobank.com>
2022-10-18 16:16:22 -0500

    Primer commit

---
+++ Pokemons-plus/Pokemons-plus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokemons-plus
+//
+//  Created by mvalencia on 18/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pokemons-plus/Pokemons-plus/Pokemons_plusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemons_plusApp.swift
+//  Pokemons-plus
+//
+//  Created by mvalencia on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Pokemons_plusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
